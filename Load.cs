987784836c373225b563bcd8999ca6681900fe33
Yosshiy <69392769+Yosshiy@users.noreply.github.com>
2021-07-28 06:21:13 +0900

    Add files via upload

---
+++ Load.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+/// <summary>
+/// Resourceから読み込む拡張メソッド
+/// </summary>
+public class Load : MonoBehaviour
+{
+    public static List<Sprite> LoadSprite(string filename)
+    {
+        var sprites = Resources.LoadAll<Sprite>(filename);
+        return sprites.ToList();
+    }
+}
